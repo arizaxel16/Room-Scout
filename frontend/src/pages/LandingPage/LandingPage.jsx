95c0a4437a7131ddc00dd54bd847a109d10612c8
arizaxel16 <110404509+arizaxel16@users.noreply.github.com>
2024-09-12 22:06:48 -0500

    CreateFiles

---
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+    return (
+        <div className='landing-page'>
+            
+        </div>
+    );
+};
+
+export default LandingPage;
