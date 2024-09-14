b128aac588e0f6ca45cd873d73d7effa4789fc4c
arizaxel16 <110404509+arizaxel16@users.noreply.github.com>
2024-09-13 22:12:17 -0500

    Update NavBar css and menu

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,44 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
-import logo_light from '../../assets/logo-b.png';
-import logo_dark from '../../assets/logo-w.png';
-import search_icon_light from '../../assets/search-w.png';
-import search_icon_dark from '../../assets/search-b.png';
-import toggle_light from '../../assets/night.png';
-import toggle_dark from '../../assets/day.png';
+import logo_light from '../../assets/LOGO_LIGHT.png';
+import logo_dark from '../../assets/LOGO_DARK.png';
+import { MdOutlineWbSunny, MdMenu } from "react-icons/md";
+import { FaRegMoon, FaSearch, FaUser } from "react-icons/fa";
 
 const NavBar = ({theme, setTheme}) => {
     const [menuOpen, setMenuOpen] = React.useState(false);
-    const toggle_mode = () => {
-        theme === 'light' ? setTheme('dark') : setTheme('light');
-    }
+    
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+
     return (
-        <nav>
-            <img src={theme === 'light' ? logo_light : logo_dark} alt="" className="logo"/>
-            <div className="menu" onClick={()=> setMenuOpen(!menuOpen)}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+        <nav className={`navbar ${theme}`}>
+            <img src={theme === 'light' ? logo_light : logo_dark} alt="logo" className="logo" />
+            
             <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+                <div className="search-box">
+                    <input type="text" placeholder="Search" />
+                    <FaSearch className="search-icon" />
+                </div>
                 <li>Home</li>
                 <li>Room</li>
                 <li>Features</li>
                 <li>About</li>
             </ul>
-            <div className={`search-box ${menuOpen ? 'open' : ''}`}>
-                <input type="text" placeholder='Search'/>
-                <img src={theme === 'light' ? search_icon_light : search_icon_dark} className='search-icon' alt=""/>
+
+            <div className="icons">
+                <MdOutlineWbSunny id='sun' className={`toggle-icon ${theme === 'light' ? '' : 'hidden'}`} onClick={toggleTheme} />
+                <FaRegMoon id='moon' className={`toggle-icon ${theme === 'dark' ? '' : 'hidden'}`} onClick={toggleTheme} />
+                <Link to="/user_auth" className="navLink-auth"><FaUser className="user-icon" /></Link>
+                <MdMenu className="menu-icon" onClick={() => setMenuOpen(!menuOpen)} />
             </div>
-            <Link to={"/user_auth"} className="navLink-auth"><button className={`register-button ${menuOpen ? 'open' : ''}`} >Register/Login</button></Link>
-
-
-            <img onClick={() => {
-                toggle_mode()
-            }} src={theme === 'light' ? toggle_light : toggle_dark} alt="" className={`toggle-icon ${menuOpen ? 'open' : ''}`}/>
-
-
         </nav>
     );
 };
