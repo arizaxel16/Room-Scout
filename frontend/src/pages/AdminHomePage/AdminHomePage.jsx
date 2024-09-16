8def17ffbb4f54cf3183dad005d5954fc2bdbe57
Juan pablo corral <78913361+juanpa-corral@users.noreply.github.com>
2024-09-16 01:00:00 -0500

    Add charts and boxes for the homepage

--- frontend/src/pages/AdminHomePage/AdminHomePage.jsx
+++ frontend/src/pages/AdminHomePage/AdminHomePage.jsx
@@ -3,6 +3,9 @@ import './AdminHomePage.scss';
 import AdminNavBar from "../../components/AdminNavBar/AdminNavBar";
 import AdminFooter from "../../components/AdminFooter/AdminFooter";
 import AdminMenu from "../../components/AdminMenu/AdminMenu";
+import TopBox from "../../components/TopBox/TopBox";
+import ChartBox from "../../components/ChartBox/ChartBox";
+import {chartBookings, chartBoxUser, chartRatio, chartRevenue} from "../../data.ts";
 
 
 const AdminHomePage = () => {
@@ -14,7 +17,17 @@ const AdminHomePage = () => {
                     <AdminMenu/>
                 </div>
                 <div className="contentContainer">
-                    HomePage
+                    <div className="home">
+                        <div className="box box1"><TopBox/></div>
+                        <div className="box box2"><ChartBox {...chartBoxUser}/></div>
+                        <div className="box box3"><ChartBox {...chartBookings}/></div>
+                        <div className="box box4">Box4</div>
+                        <div className="box box5"><ChartBox{...chartRevenue}/></div>
+                        <div className="box box6"><ChartBox{...chartRatio}/></div>
+                        <div className="box box7">Box7</div>
+                        <div className="box box8">Box8</div>
+                        <div className="box box9">Box9</div>
+                    </div>
                 </div>
             </div>
             <AdminFooter/>
