843fe409c1b80757b2e9037b10acbcf12fc7f0c3
Juan pablo corral <78913361+juanpa-corral@users.noreply.github.com>
2024-09-17 01:52:09 -0500

    Add data table and add modal

---
+++ frontend/src/components/DataTable/DataTable.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './DataTable.scss';
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import {Link} from "react-router-dom";
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+
+const DataTable = ({ columns, rows }) => {
+    const handleDelete = (id) => {
+        // Lógica para eliminar el usuario con el ID proporcionado
+        //axios.delete(`/api/users/${id}`);
+        console.log(`Eliminar usuario con ID: ${id}`);
+    };
+    // Definir la columna de acciones
+    const actionColumn = [
+        {
+            field: "action",
+            headerName: "Action",
+            width: 200,
+            renderCell: (params) => {
+                return (
+                    <div className="actionsButtons">
+                        <Link to={`/admin/users/${params.row.id}`}><FaEdit className="icon-Edit"/></Link>
+                        <div className="delete" onClick={()=>handleDelete(params.row.id)}><MdDeleteOutline className="icon-delete"/></div>
+                    </div>
+                );
+            },
+        },
+    ];
+
+    // Combinar las columnas originales con la columna de acciones
+    const allColumns = [...columns, ...actionColumn];
+
+    return (
+        <div className="dataTable">
+            <DataGrid
+                className="dataGrid"
+                rows={rows}
+                columns={allColumns}  // Usar todas las columnas
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: 5,
+                        },
+                    },
+                }}
+                slots={{ toolbar: GridToolbar }}
+                slotProps={{
+                    toolbar: {
+                        showQuickFilter: true,
+                        quickFilterProps: { debounceMs: 500 },
+                    }
+                }}
+                pageSizeOptions={[5]}
+                checkboxSelection
+                disableRowSelectionOnClick
+                disableColumnFilter={true}
+                disableColumnSelector={true}
+                disableDensitySelector={true}
+            />
+        </div>
+    );
+};
+
+export default DataTable;
--- frontend/src/pages/UserAdminPage/UserAdminPage.jsx
+++ frontend/src/pages/UserAdminPage/UserAdminPage.jsx
@@ -3,8 +3,55 @@ import './UserAdminPage.scss';
 import AdminNavBar from "../../components/AdminNavBar/AdminNavBar";
 import AdminMenu from "../../components/AdminMenu/AdminMenu";
 import AdminFooter from "../../components/AdminFooter/AdminFooter";
+import DataTable from "../../components/DataTable/DataTable";
+import Add from "../../components/Add/Add.tsx";
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+        field: 'firstName',
+        headerName: 'First name',
+        width: 150,
+        editable: true,
+    },
+
+    {
+        field: 'lastName',
+        headerName: 'Last name',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'age',
+        headerName: 'Age',
+        type: 'number',
+        width: 110,
+        editable: true,
+    },
+    {
+        field: 'fullName',
+        headerName: 'Full name',
+        description: 'This column has a value getter and is not sortable.',
+        sortable: false,
+        width: 160,
+        valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    },
+];
+
+const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 const UserAdminPage = () => {
+    const [open, setOpen] = React.useState(false);
     return (
         <div className="main">
             <AdminNavBar/>
@@ -13,7 +60,14 @@ const UserAdminPage = () => {
                     <AdminMenu/>
                 </div>
                 <div className="contentContainer">
-                    User
+                    <div className="users">
+                        <div className="info">
+                            <h1>Users</h1>
+                            <button onClick={()=>setOpen(true)}>Add New Admin User</button>
+                        </div>
+                        <DataTable columns={columns} rows={rows}/>
+                        {open && <Add slug="users" columns={columns} setOpen={setOpen}/>}
+                    </div>
                 </div>
             </div>
             <AdminFooter/>
