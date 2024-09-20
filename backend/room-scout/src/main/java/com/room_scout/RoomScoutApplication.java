a253f3fbe6311eada44008eec22e0691135d49c7
Juan pablo corral <78913361+juanpa-corral@users.noreply.github.com>
2024-09-20 07:24:28 -0500

    Add cors global config

--- backend/room-scout/src/main/java/com/room_scout/RoomScoutApplication.java
+++ backend/room-scout/src/main/java/com/room_scout/RoomScoutApplication.java
@@ -2,11 +2,23 @@ package com.room_scout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RoomScoutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoomScoutApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://157.173.114.224:3000");
+			}
+		};
 	}
 }
