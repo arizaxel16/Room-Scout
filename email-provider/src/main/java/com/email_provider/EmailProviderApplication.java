9647a36df579a42bac58ae99a41657a98244c77e
arizaxel16 <arizaxel16@gmail.com>
2024-11-22 11:23:17 -0500

    Create microservice

---
+++ email-provider/src/main/java/com/email_provider/EmailProviderApplication.java
@@ -0,0 +1,13 @@
+package com.email_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailProviderApplication.class, args);
+	}
+
+}
