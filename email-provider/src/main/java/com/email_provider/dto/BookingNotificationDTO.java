abd3da1f88569842b457f7a6ec48ac381f9f373b
arizaxel16 <arizaxel16@gmail.com>
2024-11-23 10:44:38 -0500

    Config microservice listener

---
+++ email-provider/src/main/java/com/email_provider/dto/BookingNotificationDTO.java
@@ -0,0 +1,15 @@
+package com.email_provider.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+public record BookingNotificationDTO(
+        Long id,
+        LocalDate startDate,
+        LocalDate endDate,
+        double totalPrice,
+        Long roomTypeId,
+        Long userId,
+        String email,
+        String eventType,
+        LocalDateTime eventTimestamp) {
+}
